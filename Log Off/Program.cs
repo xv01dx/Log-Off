53d730813b0c5a5403b322559f44b47c7ca0ba3f
xv01dx <Matthew Hooper@W16000>
2017-07-17 11:40:22 +0930

    Add project files.

---
+++ Log Off/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Log_Off
+{
+    class Program
+    {
+        static extern bool ExitWindowsEx(uint uFlags, uint dwReason);
+        static void Main(string[] args)
+        {
+            ExitWindowsEx(0, 0);
+        }
+    }
+}
